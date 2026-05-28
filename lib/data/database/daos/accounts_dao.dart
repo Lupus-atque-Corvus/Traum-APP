f0e0639a125081bb5cd485a8c01ed9d4de0f0db0
Lupus-atque-Corvus <lupus.atque.corvus@gmail.com>
2026-05-28 21:46:55 +0200

    feat(db): add AccountsDao

---
+++ lib/data/database/daos/accounts_dao.dart
@@ -0,0 +1,37 @@
+import 'package:drift/drift.dart';
+import '../traum_database.dart';
+
+part 'accounts_dao.g.dart';
+
+@DriftAccessor(tables: [Accounts])
+class AccountsDao extends DatabaseAccessor<TraumDatabase>
+    with _$AccountsDaoMixin {
+  AccountsDao(super.db);
+
+  Future<List<Account>> getAll() =>
+      (select(accounts)
+            ..orderBy([(a) => OrderingTerm.asc(a.sortOrder)]))
+          .get();
+
+  Stream<List<Account>> watchAll() =>
+      (select(accounts)
+            ..orderBy([(a) => OrderingTerm.asc(a.sortOrder)]))
+          .watch();
+
+  Future<Account?> getById(int id) =>
+      (select(accounts)..where((a) => a.id.equals(id))).getSingleOrNull();
+
+  Future<double> getTotalBalance() async {
+    final all = await getAll();
+    return all.fold(0.0, (sum, a) {
+      final contribution = a.type == 'credit' ? -a.balance.abs() : a.balance;
+      return sum + contribution;
+    });
+  }
+
+  Future<void> upsertAccount(AccountsCompanion account) =>
+      into(accounts).insertOnConflictUpdate(account);
+
+  Future<void> deleteAccount(int id) =>
+      (delete(accounts)..where((a) => a.id.equals(id))).go();
+}
